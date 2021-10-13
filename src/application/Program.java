85721a578535ebda93faeaaef4b680e8088c777d
Andre121193 <andre1993andre@hotmail.com>
2021-10-13 20:38:37 -0300

    Expressao lambda inline

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,6 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
 
 import entities.Product;
 
@@ -17,9 +16,7 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD case", 80.00));
 		
-		Predicate<Product> pred = p -> p.getPrice() >= 100.0;
-		
-		list.removeIf(pred);
+		list.removeIf(p -> p.getPrice() >= 100.0);
 		
 		for (Product p : list) {
 			System.out.println(p);
